538198b319ab0fdfd12c87d7b079c37e9cca5b7b
Minjae Kim <minjae.kim@mytrum.com>
2022-07-03 10:40:54 +0900

    refactor: Book Service 내부 어노테이션 추가

--- src/main/java/com/book/BookService.java
+++ src/main/java/com/book/BookService.java
@@ -1,22 +1,30 @@
 package com.book;
 
 import java.util.NoSuchElementException;
+import java.util.Objects;
 
 import com.book.annotation.BookProduceType;
 import com.book.handler.NoSuchElementExceptionHandler;
 
 import com.linecorp.armeria.common.HttpResponse;
+import com.linecorp.armeria.common.MediaTypeNames;
+import com.linecorp.armeria.server.annotation.Blocking;
+import com.linecorp.armeria.server.annotation.Consumes;
+import com.linecorp.armeria.server.annotation.ConsumesJson;
 import com.linecorp.armeria.server.annotation.Delete;
 import com.linecorp.armeria.server.annotation.ExceptionHandler;
 import com.linecorp.armeria.server.annotation.Get;
+import com.linecorp.armeria.server.annotation.Order;
 import com.linecorp.armeria.server.annotation.Param;
 import com.linecorp.armeria.server.annotation.Post;
-import com.linecorp.armeria.server.annotation.RequestObject;
+import com.linecorp.armeria.server.annotation.Produces;
+import com.linecorp.armeria.server.annotation.ProducesJson;
 import com.linecorp.armeria.server.annotation.ResponseConverter;
 import com.linecorp.armeria.server.annotation.ResponseConverters;
 
 public class BookService {
 
+    @Order(1)
     @Get("/books")
     @BookProduceType
     public HttpResponse findAll() {
@@ -33,8 +41,12 @@ public class BookService {
         return HttpResponse.ofJson(book);
     }
 
+    @Produces(MediaTypeNames.JSON)
+    @Consumes(MediaTypeNames.JSON)
+    @ProducesJson
+    @ConsumesJson
     @Post("/book")
-    public HttpResponse createBook(@RequestObject Book book) {
+    public HttpResponse createBook(Book book) {
         return HttpResponse.ofJson(BookRepository.createUpdateBook(book));
     }
 
@@ -42,12 +54,13 @@ public class BookService {
             @ResponseConverter(BookResponseConverter.class),
             @ResponseConverter(BookDeletedFailedConverter.class)
     })
+    @Blocking
     @Delete("/book/:id")
     public HttpResponse deleteById(@Param long id) {
         Book book = BookRepository.deleteById(id);
-        if (book == null) {
-            return HttpResponse.ofJson(new BookDeleteFailedResponse("failed", "this is cuase"));
-        }
-        return HttpResponse.ofJson(book);
+        return HttpResponse.ofJson(
+                Objects.requireNonNullElseGet(book, () ->
+                        new BookDeleteFailedResponse("failed",
+                                                     "this is cause")));
     }
 }
